36eea737a34c5e56aeb9f835c34b8bd5500a0f49
Jai Patel <jaipatel@Jais-MacBook-Pro.local>
2024-09-12 16:11:16 -0400

    Check

--- test_one.py
+++ test_one.py
@@ -1,6 +1,12 @@
 import math
+from app import stop_inference, cancel_inference
 
 def testsquare():
    num = 3
    assert 3*3 == 9
 
+def test_cancel():
+   global stop_inference
+   stop_inference = False
+   cancel_inference()
+   assert stop_inference is True
