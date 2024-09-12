f7ffe823c83875aeec0e9f680a6ab52e14dba26e
Jai Patel <jaipatel@Jais-MacBook-Pro.local>
2024-09-12 12:01:44 -0400

    Check

--- test_one.py
+++ test_one.py
@@ -1,13 +1,6 @@
 import math
-import gradio
-from app import stop_inference, cancel_inference
 
 def testsquare():
    num = 3
    assert 3*3 == 9
 
-def test_cancel_inference():
-   global stop_inference
-   stop_inference = False
-   cancel_inference()
-   assert stop_inference == True
